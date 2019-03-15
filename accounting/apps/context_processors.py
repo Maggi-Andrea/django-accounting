5f40660a6e88a1a6a29b9d3c1e4d7a8dd592d353
Andrea Maggi <andrea@maggicontrols.com>
2019-03-15 10:54:02 +0100

    Fixed display version

--- accounting/apps/context_processors.py
+++ accounting/apps/context_processors.py
@@ -6,10 +6,7 @@ def metadata(request):
     Add some generally useful metadata to the template context
     """
     return {
-        'display_version': getattr(settings,
-            'DISPLAY_VERSION', 'N/A'),
-        'display_short_version': getattr(settings,
-            'DISPLAY_SHORT_VERSION', 'N/A'),
-        'version': getattr(settings,
-            'VERSION', 'N/A'),
+        'display_version': getattr(settings, 'DISPLAY_VERSION', 'N/A'),
+        'display_short_version': getattr(settings, 'DISPLAY_SHORT_VERSION', 'N/A'),
+        'version': getattr(settings, 'VERSION', 'N/A'),
     }
